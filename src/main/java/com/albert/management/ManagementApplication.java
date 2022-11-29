2ddc9199b2ca3f1c5ce1bb5886a1769444a004f7
winston <winstontang@fontrip.com>
2022-11-29 17:07:57 +0800

    [modify]hello world

--- src/main/java/com/albert/management/ManagementApplication.java
+++ src/main/java/com/albert/management/ManagementApplication.java
@@ -4,8 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ManagementApplication {
   private static final Logger log = LoggerFactory.getLogger(ManagementApplication.class);
 
@@ -18,4 +22,11 @@ public class ManagementApplication {
       ManagementApplication.class.getSimpleName()
     );
   }
+
+  @GetMapping("/hello")
+  public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
+
+
 }
