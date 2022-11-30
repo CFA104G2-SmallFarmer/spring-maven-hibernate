c8ac4c2a4ce2726089fefe2e17b3706a5d3c0723
winston <winstontang@fontrip.com>
2022-11-30 17:45:55 +0800

    [modify] ignore test module

--- src/test/java/com/example/maven_demo_by_initializer/MavenDemoByInitializerApplicationTests.java
+++ src/test/java/com/example/maven_demo_by_initializer/MavenDemoByInitializerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.maven_demo_by_initializer;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MavenDemoByInitializerApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.maven_demo_by_initializer;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MavenDemoByInitializerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
